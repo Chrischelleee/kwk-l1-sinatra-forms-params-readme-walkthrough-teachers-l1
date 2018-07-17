63ec5a9250853e891c165b55deac312a5d168684
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-07-17 19:03:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ class App < Sinatra::Base
   # Add your post route and action below
   post '/food' do
     params.to_s
-  "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  # "My name is #{params[:name]}, and I love #{params[:favorite_food]}" #if you have this string after the params.to_s then it wouldn't show you the hash that they made 
+  
     end
 end
